a1bd6fe6613eda6324585977b05a238a5660e76c
Test <connectivitytest443@gmail.com>
2024-10-25 22:06:54 +0500

    create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './component/test/test.component';
+import { HelloComponent } from './component/hello/hello.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    HelloComponent
   ],
   imports: [
     BrowserModule,
